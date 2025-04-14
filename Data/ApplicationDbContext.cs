6e51bdfa9ac7f772e0f81ab71465fd7a753634c9
tanuj <tanuj@RAZER>
2025-04-14 21:10:01 +0400

    Final tests

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,5 +11,6 @@ namespace DatingManagementSystem.Data
         public DbSet<User> Users { get; set; }
         public DbSet<CompatibilityScore> CompatibilityScores { get; set; }
 
+        public DbSet<SkippedUser> SkippedUsers { get; set; }
     }
 }
